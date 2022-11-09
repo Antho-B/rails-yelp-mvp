b84fa54ca1bb52efc21649b1b673c21e12f51353
anthonyborremans <anthonyborremans@hotmail.com>
2022-11-09 12:12:45 -0500

    add model

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema[7.0].define(version: 2022_11_09_170701) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
-    t.string "adress"
+    t.string "address"
     t.string "phone_number"
     t.string "category"
     t.datetime "created_at", null: false
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_09_170701) do
   end
 
   create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
     t.text "content"
     t.integer "restaurant_id", null: false
     t.datetime "created_at", null: false
